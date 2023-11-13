65dffe51c8d1a426a700119537f9560a3d321208
afhorton <106110920+afhorton@users.noreply.github.com>
2023-11-13 09:25:30 -0700

    Update

--- src/game.jsx
+++ src/game.jsx
@@ -137,7 +137,7 @@ const Game = () => {
       <div>
         <h1>Fight!</h1>
         <div>
-         {heroAlive(true) && <div id="heroDiv">
+         {heroAlive === true && <div id="heroDiv">
             <h2>{hero.name}</h2>
             <p>HP: {hero.hp}</p>
             <p>MP: {hero.mp}</p>
@@ -147,14 +147,14 @@ const Game = () => {
             <button onClick={reManaHero}>Re-Mana</button>
             <button onClick={reGenHero}>Re-Gen</button>
           </div> ||
-          heroAlive(false) &&
+          heroAlive === false &&
           <h2>Slain!</h2> 
           }
-         { enemyAlive(true) && <div id="enemyDiv">
+         { enemyAlive === true && <div id="enemyDiv">
             <h2>{enemy.name}</h2>
             <p>HP: {enemy.hp}</p>
             <p>MP: {enemy.mp}</p>
-          </div> || enemyAlive(false) && 
+          </div> || enemyAlive === false && 
           <h2>Slain!</h2>
           }
         </div>
